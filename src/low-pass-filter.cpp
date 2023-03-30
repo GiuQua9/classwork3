08ad6813197f5aaab0976372598bfa8d54522c67
GiuQua9 <giuseppe98.quaranta@gmail.com>
2023-03-30 12:50:30 +0200

    add thread

--- src/low-pass-filter.cpp
+++ src/low-pass-filter.cpp
@@ -1,5 +1,7 @@
 
 #include "ros/ros.h"
+#include "std_msgs/Float32.h"
+#include "boost/thread.hpp"
 #include "math.h"
 #include "cwork3/sinusoid.h"
 #include "cwork3/low_pass.h"
@@ -12,17 +14,20 @@ class Low_Pass {
 	public:
 		Low_Pass();
 		void topic_cb( cwork3::sinusoid in); //callback
+		void filter();
 	
 	private:
 		ros::NodeHandle _nh;
 		ros::Subscriber _topic_sin;
 		ros::Publisher _topic_pub;
-		cwork3::low_pass _data;
+		float _in;
 		float _LPF_Beta;
+		cwork3::low_pass _out;
 };
 
 Low_Pass::Low_Pass() {
-	_data.out = 0.0;
+	_in = 0.0;
+	_out.out= 0.0;
 	_topic_pub = _nh.advertise<cwork3::low_pass>("/lowpass", 1);
 	_LPF_Beta = 1/(1+0.01); // 0<ß<1
 	//Initialize a subscriber:
@@ -31,20 +36,30 @@ Low_Pass::Low_Pass() {
 	//			Callback function
 	//			Object context: the value of data members
 	_topic_sin = _nh.subscribe("/sinusoid", 1, &Low_Pass::topic_cb, this);
+	boost::thread loop_t( &Low_Pass::filter, this);
 }
 
 //Callback function: the input of the function is the data to read
 void Low_Pass::topic_cb( cwork3::sinusoid in) {
-	static int count = 0;
-	
-	//filter
-	_data.out = _LPF_Beta*_data.out +(1-_LPF_Beta)*in.v;
 
-	ROS_INFO("sin: %.3f\t time: %.2f", _data.out, count*0.01);
-	_topic_pub.publish(_data);
-	count++;
-
+	_in = in.v;
 	
+}
+
+void Low_Pass::filter(){
+	int count = 0;
+
+	while(ros::ok()){
+
+		//filter
+		_out.out = _LPF_Beta*_out.out +(1-_LPF_Beta)*_in;
+
+		ROS_INFO("sin: %.3f\t time: %.2f", _out.out, count*0.01);
+		_topic_pub.publish(_out);
+		count++;
+		usleep(10000);
+
+	}
 }
 
 int main( int argc, char** argv ) {
